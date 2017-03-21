730eb11704b04a0fc11ade24cb7b05daff4620f5
siham18 <sihamcp@gmail.com>
2017-03-20 23:24:36 -0600

    Add Front

---
+++ FXMLExample/src/fxmlexample/FrontController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fxmlexample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Siham
+ */
+public class FrontController implements Initializable {
+
+    @FXML
+    private Button general;
+
+    @FXML public void openGeneral(){
+        
+        FXMLLoader fxml = new FXMLLoader(getClass().getResource("fxml_example.fxml"));
+        
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
