9293a714d0cfc10810ac17369897e6d6366b0266
siham18 <sihamcp@gmail.com>
2017-03-21 02:33:16 -0600

    laureate search

--- FXMLExample/src/fxmlexample/LaureateSearchController.java
+++ FXMLExample/src/fxmlexample/LaureateSearchController.java
@@ -7,12 +7,18 @@ package fxmlexample;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -40,6 +46,30 @@ public class LaureateSearchController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+     @FXML public void back(ActionEvent event) throws Exception{
+        
+       
+        FXMLLoader fxml = new FXMLLoader(getClass().getResource("Front.fxml"));
+        Parent queryResult = (Parent)fxml.load();
+        
+        Scene newScene = new Scene(queryResult);
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(newScene);
+        stage.show();
+    }
+     
+      @FXML public void openLaureate(ActionEvent event) throws Exception{
+        
+       
+        FXMLLoader fxml = new FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
+        Parent queryResult = (Parent)fxml.load();
+        
+        Scene newScene = new Scene(queryResult);
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(newScene);
+        stage.show();
+    }
     
 }
